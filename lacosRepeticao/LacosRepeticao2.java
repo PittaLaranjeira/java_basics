1aca9e5649b9eee4ad4814680f4120e0f512f10b
Vitória Laranjeira <57161648+PittaLaranjeira@users.noreply.github.com>
2022-09-11 00:06:41 -0300

    Create LacosRepeticao2.java

---
+++ lacosRepeticao/LacosRepeticao2.java
@@ -0,0 +1,16 @@
+package lacosrepeticao;
+
+public class LacosRepeticao2 {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		// imprimir a listagem de 1 a 10
+		while(i <= 10) {
+			//System.out.println(i); -> ou:
+			System.out.printf("%n %s", i);
+			i++;
+		}
+	}
+}
